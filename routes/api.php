c30369478adb8f7d036e1ae07153ba0c9b3cc45c
Allan Gabriel <allan.g531@hotmail.com>
2024-12-08 18:28:07 -0300

    Add authentication routes and user resource endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,11 +16,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth:sanctum');
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/', function () {
-    dd('Hello World');
-});
+Route::resource('users', UserController::class)->except(['create', 'edit']);
+Route::get('/cep/{cep}', [UserController::class, 'findCep']);
