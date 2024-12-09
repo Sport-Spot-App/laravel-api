425d4c8ec1541fffdeea74b09064d02d953a4672
Allan Gabriel <allan.g531@hotmail.com>
2024-12-08 21:40:42 -0300

    Refactor UserController to use UpdateUserRequest and enhance photo handling in update and destroy methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Services\ViaCepService;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
@@ -35,7 +37,7 @@ class UserController extends Controller
         $validated = $request->validated();
 
         if($request->hasFile('photo')){
-            $data['photo'] = $request->file('photo')->store('images/users', 'public');
+            $validated['photo'] = $request->file('photo')->store('images/users', 'public');
         }
 
         $user = User::create($validated);
@@ -57,23 +59,41 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, User $user)
+    public function update(UpdateUserRequest $request, User $user)
     {
-        //
+        $validated = $request->validated();
+
+        if ($request->hasFile('photo')) {
+            $data['photo'] = $request->file('photo')->store('images/users', 'public');
+        }
+        
+        $user->update($validated);
+        return response()->json(['message' => 'Usuário Atualizado com sucesso!'], $user);
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(User $user)
+    public function destroy(string $id)
     {
+        $user = User::findOrFail($id);
+
+        if(!$user){
+            return response()->json(['message' => 'Usuário não encontrado'], 404);
+        }
+
+        if($user->photo){
+            Storage::disk('public')->delete($user->photo);
+        }
         
+        $user->delete();
+        return response()->json(['message' => 'Usuário deletado com sucesso!']);
     }
 
     public function findCep(string $cep)
     {
         return $this->viaCepService->findCep($cep);
     }
-
 
 }
