f83336820acce6ddc81a6e1c964fff21f2d0e357
Allan Gabriel <allan.g531@hotmail.com>
2024-12-08 23:40:47 -0300

    Add CourtPolicy and migration for courts table

---
+++ database/migrations/2024_12_09_015304_create_courts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('zip_code');
+            $table->string('street');
+            $table->string('number');
+            $table->decimal('coordinate_x', 10, 7)->nullable();
+            $table->decimal('coordinate_y', 10, 7)->nullable();
+            $table->decimal('price_per_hour', 8, 2);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courts');
+    }
+};
