5e20d45f7369db2a636ea44086cad31a005110a7
Allan Gabriel <allan.g531@hotmail.com>
2024-12-08 21:40:19 -0300

    Add UserObserver to handle photo deletion on user update

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Storage;
+
+class UserObserver
+{
+
+    /**
+     * Handle the User "created" event.
+     */
+    public function updating(User $user)
+    {
+        if($user->getOriginal('photo') && $user->getOriginal('photo') != $user->photo) {
+            Storage::disk('public')->delete($user->getOriginal('photo'));
+        }
+    }
+}
