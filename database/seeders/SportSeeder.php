010c84f3bdd95555ba5394ef3413c13e4b5a6f8d
Allan Gabriel <allan.g531@hotmail.com>
2025-01-23 19:30:39 -0300

    (feat) added sport seed + photos

--- database/seeders/SportSeeder.php
+++ database/seeders/SportSeeder.php
@@ -12,6 +12,35 @@ class SportSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+       $sports = [
+            [
+                'name' => 'Futsal',
+                'photo' => 'https://sportspott.tech/assets/images/sports/futsal.png',
+            ],
+            [
+                'name' => 'Futebol',
+                'photo' => 'https://sportspott.tech/assets/images/sports/futebol.png',
+            ],
+            [
+                'name' => 'Vôlei',
+                'photo' => 'https://sportspott.tech/assets/images/sports/volei.png',
+            ],
+            [
+                'name' => 'Beach Tênis',
+                'photo' => 'https://sportspott.tech/assets/images/sports/beach-tenis.png',
+            ],
+            [
+                'name' => 'Tênis',
+                'photo' => 'https://sportspott.tech/assets/images/sports/tenis.png',
+            ],
+            [
+                'name' => 'Handebol',
+                'photo' => 'https://sportspott.tech/assets/images/sports/handebol.png',
+            ],
+            [
+                'name' => 'Basquete',
+                'photo' => 'https://sportspott.tech/assets/images/sports/basquete.png',
+            ],
+       ];
     }
 }
