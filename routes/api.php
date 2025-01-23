a8214b3f33c9fa83b1e747835654ac4cea836d9c
Allan Gabriel <allan@pyxpayment.com>
2025-01-23 11:51:58 -0300

    fix: add 'api' middleware to login route in api.php

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])->middleware('api');
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth:sanctum');
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
