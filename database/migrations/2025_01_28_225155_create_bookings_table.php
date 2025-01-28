c7e509285f2555f5cf24addc6ec42d9fcaf7e03d
Allan Gabriel <allan.g531@hotmail.com>
2025-01-28 20:21:46 -0300

    (feat) added model + migration booking

---
+++ database/migrations/2025_01_28_225155_create_bookings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->cascadeOnDelete()->constrained();
+            $table->foreignId('court_id')->cascadeOnDelete()->constrained();
+            $table->string('day_of_week');
+            $table->string('start_time');
+            $table->string('end_time');
+            $table->boolean('status')->default(false); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
